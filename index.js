2558505e30a5d940325301e2c4d7bc79675ff1d4
Patrick Mason <patrickmason73@gmail.com>
2022-12-06 20:02:12 -0700

    commit tree d585b8406b03ad419f98a646dc389c3bc58dfd26

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    console.log(customerName.toUpperCase())
+    console.log("customerName", customerName)
+    // console.log("customerName", customerName)
+    customerName = customerName.toUpperCase()
+}
